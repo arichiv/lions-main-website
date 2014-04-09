c3543827173529f3f78bc62f768ec376d0521fb4
Ari Chivukula <aric@umich.edu>
2014-04-08 18:26:43 -0700

    add server

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
+var http = require('http');
+var port = process.env.PORT || 5000;
+var server = http.createServer(app);
+server.listen(port);
