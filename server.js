b175f8f4030f5a4ccd9a840adaa45058d37d6dcf
Ari Chivukula <aric@umich.edu>
2014-10-14 14:32:32 -0700

    new versions

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ var robots = require('robots.txt')
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 5000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.logger('dev'));
-  app.use("/static/", express.static(__dirname + '/static/'));
-  app.use(robots(__dirname + '/robots.txt'))
-});
-
+app.set('port', process.env.PORT || 5000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use("/static/", express.static(__dirname + '/static/'));
+app.use(robots(__dirname + '/robots.txt'))
 app.get('/', function(req, res) {
   res.render("index");
 });
